0852cfed38822169d18dcee5f92937b33bac9b53
Marcin Michniak <marcin.michniak@fortedigital.no>
2018-11-08 12:03:42 +0100

    Set static data, display authors and answers randomly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <button className="btn btn-primary">bootstrap test</button>
+function  Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book written by the author shown</p>
       </div>
-    );
-  }
+    </div>)
 }
+
+function  Turn({author, books}) {
+  return (
+    <div className="row turn" style={{backgroundColor: "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="author-image" alt="Author"/>
+      </div>
+      <div className="col-6">
+        {books.map(title => <Book title={title} key={title}/>)}
+      </div>
+    </div>)
+}
+
+function  Continue() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        continue
+      </div>
+    </div>)
+}
+
+function  Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All image are from commons.wikimedia.org
+        </p>
+      </div>
+    </div>)
+}
+
+const Book = ({title}) => (
+  <div className="book">
+    <h4 className="book-title">{title}</h4>
+  </div>
+);
+
+const App = ({turnData}) => (
+  <div className="container-fluid">
+    <Hero/>
+    <Turn {...turnData} />
+    <Continue/>
+    <Footer/>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,73 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { shuffle, sample } from 'underscore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'The Adventures of Huckleberry Finn',
+            'Life on the Mississippi',
+            'Roughing It'
+        ]
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: [
+            'The Shining',
+            'It'
+        ]
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/shakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Romeo and Juliet',
+            'Hamlet',
+            'Otello',
+            'Macbeth'
+        ]
+    }
+];
+
+const getTurnData = authors => {
+    const allBooks = authors.reduce((p, c, i) => p.concat(c.books), []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+            author.books.some((title) => 
+                title === answer))
+    }
+};
+
+const state = {
+    turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
