726db214323caf639c8b34ceeb99dddba331eb29
Marcin Michniak <marcin.michniak@fortedigital.no>
2018-11-15 13:46:59 +0100

    Add React-Redux. Use redux for App and AuthorQuiz components.

--- src/App.jsx
+++ src/App.jsx
@@ -3,109 +3,108 @@ import AuthorQuiz from './Components/AuthorQuiz';
 import { shuffle, sample } from 'underscore';
 import { BrowserRouter, Route, Switch, withRouter } from 'react-router-dom';
 import AddAuthorForm from './Components/AddAuthorForm';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
 import './App.css';
 
 
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            turnData: this.getTurnData(this.authors),
-            highlight: '',
-            onAnswerSelected: this.onAnswerSelected,
-            onContinue: () => this.resetState()
-        };
-    } 
+const App = () => ( 
+    <ReactRedux.Provider store={store} >
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/"  component={AuthorQuiz}/>
+                <Route path="/add" component={AuthorWrapper} />
+            </Switch>
+        </BrowserRouter>
+    </ReactRedux.Provider>
+);
 
-    resetState = () => this.setState({turnData: this.getTurnData(this.authors), highlight: ''});
-
-    authors = [
-        {
-            name: 'Mark Twain',
-            imageUrl: 'images/authors/marktwain.jpg',
-            imageSource: 'Wikimedia Commons',
-            books: [
-                'The Adventures of Huckleberry Finn',
-                'Life on the Mississippi',
-                'Roughing It'
-            ]
-        },
-        {
-            name: 'Joseph Conrad',
-            imageUrl: 'images/authors/josephconrad.jpg',
-            imageSource: 'Wikimedia Commons',
-            books: ['Heart of Darkness']
-        },
-        {
-            name: 'J.K. Rowling',
-            imageUrl: 'images/authors/jkrowling.jpg',
-            imageSource: 'Wikimedia Commons',
-            imageAttribution: 'Daniel Ogren',
-            books: ['Harry Potter and the Sorcerers Stone']
-        },
-        {
-            name: 'Stephen King',
-            imageUrl: 'images/authors/stephenking.jpg',
-            imageSource: 'Wikimedia Commons',
-            imageAttribution: 'Pinguino',
-            books: [
-                'The Shining',
-                'It'
-            ]
-        },
-        {
-            name: 'William Shakespeare',
-            imageUrl: 'images/authors/shakespeare.jpg',
-            imageSource: 'Wikimedia Commons',
-            books: [
-                'Romeo and Juliet',
-                'Hamlet',
-                'Otello',
-                'Macbeth'
-            ]
-        }
-    ];
-    
-    getTurnData = authors => {
-        const allBooks = authors.reduce((p, c, i) => p.concat(c.books), []);
-        const fourRandomBooks = shuffle(allBooks).slice(0, 4);
-        const answer = sample(fourRandomBooks);
-    
-        return {
-            books: fourRandomBooks,
-            author: authors.find((author) => 
-                author.books.some((title) => 
-                    title === answer))
-        }
-    };
-    
-    onAnswerSelected = answer => {
-        const isCorrect = this.state.turnData.author.books.some(book => book === answer);
-        this.setState({
-            highlight: isCorrect ? 'correct' : 'wrong'
-        });
-    };
-
-    AuthorWrapper = withRouter(({ history }) => 
-        <AddAuthorForm onAddAuthor={author => {
-            author.imageSource = author.imageSource ? author.imageSource : "Unknown";
-            this.authors.push(author);
-            history.push('/');
-        }}/>
-    );
-
-    render() {
-        return ( 
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/"  render={() => <AuthorQuiz {...this.state}/>}/>
-                    <Route path="/add" component={this.AuthorWrapper} />
-                </Switch>
-            </BrowserRouter>
-        );
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/marktwain.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'The Adventures of Huckleberry Finn',
+            'Life on the Mississippi',
+            'Roughing It'
+        ]
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/josephconrad.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jkrowling.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Daniel Ogren',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephenking.jpg',
+        imageSource: 'Wikimedia Commons',
+        imageAttribution: 'Pinguino',
+        books: [
+            'The Shining',
+            'It'
+        ]
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/shakespeare.jpg',
+        imageSource: 'Wikimedia Commons',
+        books: [
+            'Romeo and Juliet',
+            'Hamlet',
+            'Otello',
+            'Macbeth'
+        ]
     }
+];
 
-}
+const getTurnData = authors => {
+    const allBooks = authors.reduce((p, c, i) => p.concat(c.books), []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) => 
+            author.books.some((title) => 
+                title === answer))
+    }
+};
+
+const reducer = (state = { authors: authors, turnData: getTurnData(authors), highlight: ''}, action) => {
+    switch (action.type) {
+        case 'ANSWER_SELECTED':
+            const isCorrect = state.turnData.author.books.some(book => book === action.answer);
+            return Object.assign({}, state, { 
+                highlight: isCorrect ? 'correct' : 'wrong' 
+            });
+        case 'CONTINUE':
+            return Object.assign({}, state, { 
+                highlight: '', 
+                turnData: getTurnData(state.authors)
+            });
+        default:
+            return state;
+    }
+};
+
+const store = Redux.createStore(reducer);
+
+const AuthorWrapper = withRouter(({ history }) => 
+    <AddAuthorForm onAddAuthor={author => {
+        author.imageSource = author.imageSource ? author.imageSource : "Unknown";
+        authors.push(author);
+        history.push('/');
+    }}/>
+);
 
 export default App;
--- src/Components/AuthorQuiz.jsx
+++ src/Components/AuthorQuiz.jsx
@@ -4,8 +4,27 @@ import Hero from './Hero';
 import Continue from './Continue';
 import Footer from './Footer';
 import AddAuthorButton from './AddAuthorButton';
+import { connect } from 'react-redux';
 
-const AuthorQuiz = ({turnData, highlight, onAnswerSelected, onContinue}) => (
+const mapStateToProps = (state) => {
+  return {
+    turnData: state.turnData, 
+    highlight: state.highlight
+  };
+};
+
+const mapDispatchToProps = (dispatchEvent) => {
+  return {
+    onAnswerSelected: (answer) => {
+      dispatchEvent({ type: 'ANSWER_SELECTED', answer });
+    },
+    onContinue: () => {
+      dispatchEvent({ type: 'CONTINUE' });
+    }
+  };
+};
+
+const AuthorQuiz = connect(mapStateToProps, mapDispatchToProps)(({turnData, highlight, onAnswerSelected, onContinue}) => (
   <div className="container-fluid">
     <Hero/>
     <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
@@ -13,6 +32,6 @@ const AuthorQuiz = ({turnData, highlight, onAnswerSelected, onContinue}) => (
     <AddAuthorButton/>
     <Footer/>
   </div>
-);
+));
 
 export default AuthorQuiz;
